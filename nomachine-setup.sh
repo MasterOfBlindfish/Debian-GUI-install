6511edb8b7a4d5da005b7351896f2c5b841e87ce
MasterOfBlindfish <45813612+MasterOfBlindfish@users.noreply.github.com>
2026-05-28 11:27:17 +0200

    Update nomachine-setup.sh

--- nomachine-setup.sh
+++ nomachine-setup.sh
@@ -7,7 +7,9 @@ cd /tmp
 wget https://download.nomachine.com/download/latest/linux/nomachine_amd64.deb
 
 echo "=== Installing NoMachine ==="
-sudo dpkg -i nomachine_amd64.deb || sudo apt -f install -y
+# sudo dpkg -i nomachine_amd64.deb || sudo apt -f install -y
+sudo apt install links2
+links2 nomachine.com
 
 echo "=== Enabling service ==="
 sudo systemctl enable nxserver
